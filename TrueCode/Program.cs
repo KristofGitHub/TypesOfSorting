2912c955355b68ad24c981ff96d84848a9c23980
Vasiliev Sergei <sergei.binp@gmail.com>
2023-07-12 18:16:19 +0500

    1st commit: begin...

--- TrueCode/Program.cs
+++ TrueCode/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ListOfSorting
+{
+    public class ListOfSortingAlgs
+    {
+        static void Swap(ref int leftValue, ref int rightValue)
+        {
+            int temp = leftValue;
+            leftValue = rightValue;
+            rightValue = temp;
+        } // Конец метода Swap
+        static void QuickSort(int[] inputArray, int minIndex, int maxIndex)
+        {
+            static int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+            {
+                int PivotIndex = minIndex - 1;
+                for(int i = minIndex; i <= maxIndex; i++)
+                {
+                    if(inputArray[i] < inputArray[maxIndex]) 
+                    {
+
+                    } // Конец условного оператора if
+                } // Конец цикла for
+            } // Конец метода GetPivotIndex
+        } // Конец метода QuickSort
+
+    } // Конец класса ListOfSortingAlgs
+} // Конец пространства имен (модуля, библиотеки, коллекции классов) ListOfSorting
