25356f2955715c7eacd9b8de53089fa0236bdd81
Vasiliev Sergei <sergei.binp@gmail.com>
2023-07-12 18:34:55 +0500

    2nd commit: Quick sorting method is ready

--- TrueCode/Program.cs
+++ TrueCode/Program.cs
@@ -12,16 +12,28 @@
         {
             static int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
             {
-                int PivotIndex = minIndex - 1;
-                for(int i = minIndex; i <= maxIndex; i++)
+                int pivotIndex = minIndex - 1;
+                for (int i = minIndex; i <= maxIndex; i++)
                 {
-                    if(inputArray[i] < inputArray[maxIndex]) 
+                    if (inputArray[i] < inputArray[maxIndex]) 
                     {
-
+                        pivotIndex++;
+                        Swap(ref inputArray[i], ref inputArray[pivotIndex]);
                     } // Конец условного оператора if
                 } // Конец цикла for
+                pivotIndex++;
+                Swap(ref inputArray[pivotIndex], ref inputArray[maxIndex]);
+                return pivotIndex;
             } // Конец метода GetPivotIndex
-        } // Конец метода QuickSort
-
+            if (minIndex >= maxIndex) return;
+            int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+            QuickSort(inputArray, minIndex, pivot - 1);
+            QuickSort(inputArray, pivot + 1, maxIndex);
+            return;
+        } // Конец метода QuickSort (Быстрой сортировки)
+        public static void BubbleSort(int[] inputArray)
+        {
+            
+        } // Конец метода BubbleSort (Пузырьковой сортировки)
     } // Конец класса ListOfSortingAlgs
-} // Конец пространства имен (модуля, библиотеки, коллекции классов) ListOfSorting
+} // Конец пространства имен (модуля, библиотеки, коллекции классов, что-то такое) ListOfSorting
