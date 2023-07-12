696a0094e7ee5b33e9ef4ab1684cdcdd6db8b7ee
Vasiliev Sergei <sergei.binp@gmail.com>
2023-07-12 19:53:42 +0500

    5th commit: Counting sorting method is ready

--- TrueCode/Program.cs
+++ TrueCode/Program.cs
@@ -21,8 +21,8 @@
                     {
                         pivotIndex++;
                         Swap(ref inputArray[i], ref inputArray[pivotIndex]);
-                    } // Конец условного оператора if
-                } // Конец цикла for
+                    }
+                }
                 pivotIndex++;
                 Swap(ref inputArray[pivotIndex], ref inputArray[maxIndex]);
                 return pivotIndex;
@@ -41,10 +41,10 @@
                 {
                     if (inputArray[j] > inputArray[j+1]) 
                     {
-                        Swap(ref  inputArray[j], ref inputArray[j+1]);
-                    } // Конец условного оператора if
-                }  // Конец вложенного цикла for
-            }  // Конец цикла for
+                        Swap(ref inputArray[j], ref inputArray[j+1]);
+                    }
+                }
+            }
         } // Конец метода BubbleSort (Пузырьковой сортировки) ///////////////////////////////////////////////////////////////////////////
         public static void SelectionSort(int[] inputArray)
         {
@@ -56,9 +56,33 @@
                     if (inputArray[j] < inputArray[min])
                     {
                         min = j;
-                    } // Конец условного оператора if
-                }  // Конец вложенного цикла for
-            }  // Конец цикла for
+                    }
+                }
+            }
         } // Конец метода SelectionSort (Сортировки выбором) ///////////////////////////////////////////////////////////////////////////
+        public static void CountingSort(int[] inputArray)
+        {
+            var min = inputArray[0];
+            var max = inputArray[0];
+            foreach (int element in inputArray)
+            {
+                if (element > max) {max = element;}
+                else if (element < min) {min = element;}
+            }
+            var correctionFactor = min != 0 ? -min : 0;
+            max += correctionFactor;
+            var count = new int[max+1];
+            for (var i = 0; i < inputArray.Length; i++) {count[inputArray[i] + correctionFactor]++;}
+            var index = 0;
+            for (var i = 0; i < count.Length; i++) 
+            {
+                for (var j = 0; j <count[i]; j++) 
+                {
+                    inputArray[index] = i - correctionFactor;
+                    index++;
+                }
+            }
+            return;
+        } // Конец метода CountingSort ////////////////////////////////////////////////////////////////////////////////////////////////
     } // Конец класса ListOfSortingAlgs
 } // Конец пространства имен (модуля, библиотеки, коллекции классов, что-то такое) ListOfSorting
